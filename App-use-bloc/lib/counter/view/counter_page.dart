fe15ae6506282d7beda7d0e4bb5a849543e2967f
quang ha <aibiettoi19@gmail.com>
2022-06-12 20:48:27 +0700

    Counter app new

--- App-use-bloc/lib/counter/view/counter_page.dart
+++ App-use-bloc/lib/counter/view/counter_page.dart
@@ -5,12 +5,8 @@ import '../counter.dart';
 import '../cubit/counter_cubit.dart';
 import 'counter_view.dart';
 
-/// {@template counter_page}
-/// A [StatelessWidget] which is responsible for providing a
-/// [CounterCubit] instance to the [CounterView].
-/// {@endtemplate}
 class CounterPage extends StatelessWidget {
-  /// {@macro counter_page}
+
   const CounterPage({Key? key}) : super(key: key);
 
   @override
